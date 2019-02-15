ea5687ea1110bfe04db5b1e752691f0a31e56aa1
BigmanVin <47624484+BigmanVin@users.noreply.github.com>
2019-02-15 00:04:11 -0800

    New commit

---
+++ HighScorePosition.java
@@ -0,0 +1,81 @@
+public class HighScorePosition {
+
+    public static void main(String[] args) {
+
+
+        int highScorePosition = calculateHighScorePosition(1000);
+        displayHighScorePosition("John", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(900);
+        displayHighScorePosition("Bob", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(400);
+        displayHighScorePosition("Percy", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(50);
+        displayHighScorePosition("Gilbert", highScorePosition);
+
+    }
+
+
+    public static void displayHighScorePosition (String playerName, int highScoreposition) {
+        System.out.println("Player " + playerName + " managed to get into position " + highScoreposition + " on the high score table");
+    }
+
+    public static int calculateHighScorePosition (int playerScore) {
+//		if (playerScore >= 1000) {
+//			return 1;
+//		} else if (playerScore >= 500) {
+//			return 2;
+//		} else if (playerScore >= 100) {
+//			return 3;
+//		}
+//			return 4;
+
+        int position = 4;
+
+        if (playerScore >=1000) {
+            position = 1;
+        } else if (playerScore >=500) {
+            position = 2;
+        } else if (playerScore >=100) {
+            position = 3;
+        }
+        return position;
+    }
+
+
+}
+
+
+
+//boolean gameOver = true;
+//int score = 800;
+//int levelCompleted = 5;
+//int bonus = 100;
+//
+//int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+//System.out.println("Your final score was " + highScore);
+//
+//score = 10000;
+//levelCompleted = 8;
+//bonus = 200;
+//
+//highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+//System.out.println("Your final score was " + highScore);
+//
+//
+//}
+//
+//public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+//
+//if(gameOver) {
+//	int finalScore = score + (levelCompleted * bonus);
+//	finalScore += 2000;
+//
+//	return finalScore;
+//}
+//
+//return -1;
+
+
